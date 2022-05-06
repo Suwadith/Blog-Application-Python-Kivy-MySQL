cd2e6b80ceb4def9403582808a92ced30a20300c
Suwadith <suwadith.srithar@gmail.com>
2022-05-06 14:21:15 +0100

    Public posts are readable

--- Demo/Toolbar/Toolbar.py
+++ Demo/Toolbar/Toolbar.py
@@ -1,39 +1,90 @@
+import base64
+import time
+
 from kivy.lang import Builder
-from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import ScreenManager
 from kivymd.app import MDApp
-from kivy.factory import Factory
-# from kivymd.uix.snackbar import Snackbar
+from kivymd.uix.card import MDCard, MDSeparator
 from kivymd.uix.label import MDLabel
 from kivymd.uix.screen import MDScreen
 
 import database_handler
 
 
+class PublicScreen(MDScreen):
+
+    def load_posts(self):
+        results = database_handler.get_public_blog_post()
+        for post in results:
+            post_card = MDCard(
+                orientation="vertical",
+                padding="20dp",
+                size_hint=[None, None],
+                size=["500dp", "500dp"],
+                pos_hint={"center_x": .5, "center_y": .5},
+                # size=self.size
+                # adaptive_height=True
+            )
+            post_title = MDLabel(
+                halign="center",
+                text=post[2],
+                theme_text_color="Secondary",
+                size_hint_y=None,
+                height="40dp"
+            )
+            title_separator = MDSeparator(
+                height="1dp"
+            )
+            post_timestamp = MDLabel(
+                text=str(post[1]),
+                halign="right",
+                theme_text_color="Secondary",
+                # pos_hint={"top": 0}
+                size_hint_y=None,
+                height="20dp"
+            )
+            post_body = MDLabel(
+                text=post[3],
+                # size_hint_y=None
+                # height='10dp'
+            )
+            # post_attachment = MDLabel(text=(base64.b64decode(post[4])))
+            author_name = MDLabel(
+                text=post[6],
+                halign="right",
+                theme_text_color="Secondary",
+                size_hint_y=None,
+                height="20dp"
+            )
+
+
+            post_card.add_widget(post_title)
+            post_card.add_widget(title_separator)
+            post_card.add_widget(post_timestamp)
+            post_card.add_widget(post_body)
+            post_card.add_widget(author_name)
+            # post_card.add_widget(post_attachment)
+
+            self.ids.posts.add_widget(post_card)
+
+
+class MemberScreen(MDScreen):
+    pass
+
+
+class ScreenManager(ScreenManager):
+    pass
+
+
+sm = ScreenManager()
+sm.add_widget(PublicScreen(name='public'))
+sm.add_widget(MemberScreen(name='member'))
+
+
 class BlogAndJournal(MDApp):
+
     def build(self):
         return Builder.load_file('Toolbar.kv')
-
-    def public_home_page(self):
-
-        # Snackbar(text="This is a snackbar!").open()
-        results = database_handler.get_public_blog_post()
-        print(results)
-        print("Public")
-
-    def members_home_page(self):
-        print("Members")
-
-    def private_home_page(self):
-        print("Private")
-
-    def create_post(self):
-        print("Create Post")
-
-    def admin_page(self):
-        print("Admin")
-
-    def login_logout(self):
-        print("Login/Logout")
 
 
 BlogAndJournal().run()
